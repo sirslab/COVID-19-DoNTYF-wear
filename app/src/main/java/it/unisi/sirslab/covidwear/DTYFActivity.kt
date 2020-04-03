3c316c7e35f0756a6a566561d3d8e1b503e3b3b5
Tommaso LB <lisini@diism.unisi.it>
2020-04-03 22:07:37 +0200

    speriamo vai

--- app/src/main/java/it/unisi/sirslab/covidwear/DTYFActivity.kt
+++ app/src/main/java/it/unisi/sirslab/covidwear/DTYFActivity.kt
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2020 SIRSLab - University of Siena  <Gianluca, Nicole, Tommaso>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package it.unisi.sirslab.covidwear
 
 import android.Manifest
@@ -156,7 +173,8 @@ class DTYFActivity : WearableActivity(), SensorEventListener, View.OnClickListen
             vibrator .vibrate(vibrationLength.toLong())
             lastVibTime = t
             if (lastNotificationTime+2000 < t) {
-                val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                val notification: Uri =
+                    RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
                 val r = RingtoneManager.getRingtone(applicationContext, notification)
                 r.play()
                 lastNotificationTime = t
