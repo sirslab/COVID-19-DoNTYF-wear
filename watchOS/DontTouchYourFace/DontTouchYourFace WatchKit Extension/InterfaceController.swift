238b2e23c33b737d74bd5e6612b14eef51cc32fd
ndPPPhz <andp92@gmail.com>
2020-04-06 14:42:23 +0100

    Create watchOS project
    Remove
    
    Remove

---
+++ watchOS/DontTouchYourFace/DontTouchYourFace WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DontTouchYourFace WatchKit Extension
+//
+//  Created by Annino De Petra on 06/04/2020.
+//  Copyright © 2020 Annino De Petra. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
