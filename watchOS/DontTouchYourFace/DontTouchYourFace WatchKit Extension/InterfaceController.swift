110d2408118677be56734b133716dbe129dcd7ae
ndPPPhz <andp92@gmail.com>
2020-04-06 14:57:50 +0100

    Read raw rawMagneticField values and prints them on the main page

--- watchOS/DontTouchYourFace/DontTouchYourFace WatchKit Extension/InterfaceController.swift
+++ watchOS/DontTouchYourFace/DontTouchYourFace WatchKit Extension/InterfaceController.swift
@@ -8,11 +8,13 @@
 
 import WatchKit
 import Foundation
+import CoreMotion
 
+final class InterfaceController: WKInterfaceController {
+	@IBOutlet private var dataLabel: WKInterfaceLabel!
+	private let coreMotionManager = CMMotionManager()
 
-class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
+	override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
@@ -21,11 +23,32 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+		coreMotionManager.startMagnetometerUpdates(to: .main) { [weak self] (data, error) in
+			guard let _self = self else {
+				return
+			}
+
+			// Magnetometer's outcome is an error
+			if let error = error {
+				_self.dataLabel.setText("Error: \(error.localizedDescription)")
+				return
+			}
+
+			// Magnetometer's outcome is a valid measurement
+			guard let data = data else {
+				print("Error is not nil but no data available!")
+				return
+			}
+
+			// Raw magnetic field. It means it reports
+			// the result of the measurement without filtering out the bias introduced by the device and, in some cases, its surrounding fields.
+			let rawMagneticField = data.magneticField
+			_self.dataLabel.setText("x: \(rawMagneticField.x), y: \(rawMagneticField.y), z: \(rawMagneticField.z)")
+		}
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
 }
